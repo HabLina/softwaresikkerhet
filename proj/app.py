3d92664d0617a2cbc64195f71df767b57c85c146
Lina Habaieb <linaha@uia.no>
2024-11-10 15:22:17 +0100

    Added registration with hashing and salting

--- proj/app.py
+++ proj/app.py
@@ -1,29 +1,92 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 import bleach
+import hashlib
+import bcrypt
 app = Flask(__name__)
 
 
-def init_db():
+def clear_db():
     conn = sqlite3.connect("data.db")
     c = conn.cursor()
+
+    conn.commit()
+    conn.close()
+
+
+def insert_default_data():
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+
+    # Insert default data (adjust based on your data)
+    c.execute("DROP TABLE IF EXISTS entries;")
     c.execute(
         """
         CREATE TABLE IF NOT EXISTS entries (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
-            email TEXT NOT NULL,
-            message TEXT NOT NULL
+            email TEXT NOT NULL UNIQUE,
+            message TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
     """
     )
+    c.execute("DROP TABLE IF EXISTS users;")
+    c.execute(
+        """
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+        )
+    """
+    )
+
+    c.execute("INSERT INTO entries (name, email, message) VALUES (?, ?, ?)", ("Lejla", "lejlam@uia.no", "Cowboy-Laila"))
+    c.execute("INSERT INTO entries (name, email, message) VALUES (?, ?, ?)", ("Lina", "linaha@uia.no", "Lol"))
+    c.execute("INSERT INTO entries (name, email, message) VALUES (?, ?, ?)", ("Julia", "juliamm@uia.no", "McLaren"))
+
+    c.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", ("admin1", "admin@uia.no", "admin1."))
+    
     conn.commit()
     conn.close()
 
 
+def init_db():
+    clear_db()
+    insert_default_data()
+
+@app.route("/register", methods=["GET","POST"])
+def registerAccount():
+    if request.method == "POST":
+        username = bleach.clean(request.form["username"])
+        email = bleach.clean(request.form["email"])
+        password = bleach.clean(request.form["password"])
+
+        # Hash the password
+        combine = (bcrypt.gensalt(rounds=12) + password.encode())
+        hashedPassword = hashlib.sha256(combine).hexdigest()
+
+
+        # Insert data into SQLite database
+        conn = sqlite3.connect("data.db")
+        c = conn.cursor()
+        c.execute(
+            "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+            (username, email, hashedPassword),
+        )
+        conn.commit()
+        conn.close()
+        return redirect(url_for("index"))
+
+    return render_template("register.html")
+
+
 @app.route("/")
 def index():
-    return render_template("form.html")
+    return render_template("main.html")
 
 
 @app.route("/submit", methods=["POST"])
